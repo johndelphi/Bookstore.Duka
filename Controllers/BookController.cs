5b9a41678b06b16da8a78df7e07ebe07407179e8
jomo4jo@hotmail.com <jomo4jo@hotmail.com>
2021-01-11 14:30:39 -0600

    added files from book store 1

---
+++ Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using Bookstore.Models;
+using Bookstore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+  public class BookController : Controller
+  {
+    private readonly BookRepository bookRepository = null;
+    public BookController()
+    {
+      bookRepository = new BookRepository();
+    }
+   public ViewResult GetAllBooks()
+    { var AllBooks = bookRepository.GetBooks();
+      return View(AllBooks);
+    }
+    public ViewResult GetBooks(int id)
+    {
+     var data= bookRepository.GetBookById(id);
+      return View(data);
+    }
+    public List <Books> SearchBooks(string bookName,string authorName)
+    {
+      return bookRepository.SearchBook(bookName, authorName);
+    }
+    
+  }
+}
---
+++ Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using Bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Repository
+{
+  public class BookRepository
+  {
+    public List<Books> GetBooks()
+    {
+      return DataSource();
+    }
+    public Books GetBookById(int id)
+    {
+      return DataSource().Where(X => X.Id == id).FirstOrDefault();
+
+    }
+    public List<Books>SearchBook(string title,string authorName)
+    {
+      return DataSource().Where(x => x.Title.Contains (title) && x.Author.Contains(authorName)).ToList();
+    }
+    public List<Books> DataSource()
+    {
+      return new List<Books>()
+      {
+        new Books(){Id=1,Title="phyton",Author="teric", Description="This is a phython book data science realated",Category="programming",Totalpages=100,Language="English"},
+        new Books(){Id=2,Title="macs",Author="longhorn",Description="This book is on trouble shooting mac books and repair",Category="guides",Totalpages=89,Language="spanish"},
+        new Books(){Id=3,Title="calculas",Author="thomson",Description="This book is on college math, practice problems and solutions",Totalpages=66, Category="math",Language="swahili" },
+         new Books(){Id=3,Title="Algebra",Author="thomson",Description="This book is on college math, practice problems and solutions",Totalpages=66, Category="math",Language="swahili"},
+          new Books(){Id=3,Title="calculas",Author="thomson",Description="This book is on college math, practice problems and solutions",Totalpages=66, Category="math",Language="swahili"}
+      };
+    }
+  }
+}
